105df1da8027440ccfa07b8120a74883b0100634
JessicaOFerreira <51684387+JessicaOFerreira@users.noreply.github.com>
2020-08-06 21:53:03 -0300

    Update listener.js

--- ATPR REMOTA 01- BOM/js/listener.js
+++ ATPR REMOTA 01- BOM/js/listener.js
@@ -6,50 +6,46 @@ let address = document.querySelector(".address");
 let closeBtn = document.querySelector(".close_button");
 let tools = document.querySelector("#tools");
 let browser = document.querySelector("#browser");
-let iframe;
+
 //--------------------------------------------------
 
 irBtn.addEventListener("click", function (e) {
     e.preventDefault(e);
-    
+
     if (address.value == "" || address.value == " ") {
-        
+
         alert("Campo de Endereco Obrigatorio!");
-    
-    }
-
-    else{
-        
-        let value = address.value;
-        iframe = open(value, "frameTab");
-
-        //Menu não funciona
-        menu.addEventListener("click", function(){
-            alert("Informacoes Gerais da Pagina: \n" + "URL: " + iframe.href);
-        });
 
     }
-    
-}); 
+
+    let value = address.value;
+    let iframe = open(value, "frameTab");
+
+    menu.addEventListener("click", function () {
+        alert("Informacoes Gerais da Pagina: \n" + "URL: " + iframe.href);
+    });
+
+});
 
 
-closeBtn.addEventListener("click", function(){
-    tools.style.display="none";
+closeBtn.addEventListener("click", function () {
+    tools.style.display = "none";
 
-    let newWindow= document.createElement("button")
-    newWindow.id="newWindow";
-    newWindow.insertAdjacentHTML("afterbegin","Nova Janela")
+    let newWindow = document.createElement("button")
+    newWindow.id = "newWindow";
+    newWindow.insertAdjacentHTML("afterbegin", "Nova Janela")
 
     browser.append(newWindow);
 
-    iframe = open("about:blank", "frameTab");
+    open("about:blank", "frameTab");
 
     address.value = "";
 
-    newWindow.addEventListener("click", function(){
+    newWindow.addEventListener("click", function () {
         browser.removeChild(newWindow);
-        tools.style.display="block";  
-        iframe = open("./readme/readme.html", "frameTab")
+        tools.style.display = "block";
+
+        open("./readme/readme.html", "frameTab")
     });
 });
 
